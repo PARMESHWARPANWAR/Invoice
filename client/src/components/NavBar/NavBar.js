f2476bca1dc0200b20f631d50ac9aa4edaf800d8
Parmeshwar <parmeshwarpanwar777@gmail.com>
2022-12-17 10:56:12 +0530

    fix a bug

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 
-function NavBar() {
+const NavBar = () => {
   const location = useLocation();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
@@ -16,12 +16,12 @@ function NavBar() {
       <nav className="navbar">
         <ul className="navbar-nav">
           <li className="logo">
-            <Link to="/" className="nav-link">
+            <a href="/" className="nav-link">
               <span className="link-text logo-text">
                 <img
                   style={{ width: "50px" }}
-                  src=""
-                  alt="Parmeshwar-Invoice"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvAgK0SNpeAlWuiFG_zYKt8WuLIRmJeyXhw0BkxaU&s"
+                  alt="arc-invoice"
                 />
               </span>
               <svg
@@ -39,7 +39,7 @@ function NavBar() {
                 <line x1="5" y1="12" x2="19" y2="12"></line>
                 <polyline points="12 5 19 12 12 19"></polyline>
               </svg>
-            </Link>
+            </a>
           </li>
           <li className="nav-item">
             <Link to="/" className="nav-link">
@@ -64,26 +64,28 @@ function NavBar() {
 
           <li className="nav-item">
             <Link to="/invoice" className="nav-link">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-plus"
-              >
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <line x1="5" y1="12" x2="19" y2="12"></line>
-              </svg>
-              <span className="link-text">Create</span>
+              <a href="/invoice" className="nav-link">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-plus"
+                >
+                  <line x1="12" y1="5" x2="12" y2="19"></line>
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+                <span className="link-text">Create</span>
+              </a>
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/invoices" className="nav-link">
+            <a href="/invoices" className="nav-link">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -101,10 +103,10 @@ function NavBar() {
                 <polyline points="2 12 12 17 22 12"></polyline>
               </svg>
               <span className="link-text">Invoices</span>
-            </Link>
+            </a>
           </li>
           <li className="nav-item">
-            <Link to="/customers" className="nav-link">
+            <a href="/customers" className="nav-link">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -123,10 +125,11 @@ function NavBar() {
                 <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
               </svg>
               <span className="link-text">Customers</span>
-            </Link>
+            </a>
           </li>
+
           <li className="nav-item" id="themeButton">
-            <Link to="/settings" className="nav-link">
+            <a href="/settings" className="nav-link">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -143,12 +146,12 @@ function NavBar() {
                 <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
               </svg>
               <span className="link-text">Settings</span>
-            </Link>
+            </a>
           </li>
         </ul>
       </nav>
     </div>
   );
-}
+};
 
 export default NavBar;
